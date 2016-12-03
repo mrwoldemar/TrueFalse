53c48bce9f25fd3a1fd7d2901220f14caab38589
mrwoldemar <v526@me.com>
2016-12-03 18:09:17 +0300

    Initial commit

---
+++ app/src/main/java/com/example/student3/truefalse/TrueFalse.java
@@ -0,0 +1,17 @@
+package com.example.student3.truefalse;
+
+/**
+ * Created by student3 on 03.12.16.
+ */
+public class TrueFalse {
+    private String quest;
+    private  boolean answer;
+
+    public String getQuest(String a,String b){
+        return quest;
+    }
+
+    public boolean isAnswer(String a,String b){
+        return answer;
+    }
+}
